c8b029a9d352747638cb98ac92e52ea23acbe39a
Pg28fyiW <131163094+Pg28fyiW@users.noreply.github.com>
2024-05-12 00:49:27 +0900

    Add files via upload

---
+++ todo-api/src/main/java/com/example/todoapi/controller/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.example.todoapi.controller;
+
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+@Value
+public class SampleDTO {
+
+      String content;
+
+     LocalDateTime timestamp;
+
+}
